5167d91ef5fbf05d6a6f9a36c277eeef86d7e551
lym <1948935076@qq.comm>
2022-08-03 16:11:34 +0800

    feat: html page

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const Koa = require('koa');
 const app = new Koa();
-
+// const views = require('koa-views');
+const statics = require('koa-static');
+app.use(statics(__dirname + '/views'), {extensions: ['html']})
+// 1.静态资源服务，指定对外提供访问的根目录，不包括 public
+// app.use(statics(__dirname + '/public'));
 // x-response-time
 //
 // app.use(async (ctx, next) => {
@@ -21,14 +25,14 @@ const app = new Koa();
 
 // response
 
-app.use(async ctx => {
-	ctx.body = {
-		code: 200,
-		success: true,
-		data: {
-			text: 'Hello Docker 123'
-		}
-	};
-});
+// app.use(async ctx => {
+// 	ctx.body = {
+// 		code: 200,
+// 		success: true,
+// 		data: {
+// 			text: 'Hello Docker 123'
+// 		}
+// 	};
+// });
 
 app.listen(8080);
